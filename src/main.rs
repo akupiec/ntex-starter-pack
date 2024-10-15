6eddb75514022762e01a52c0f27145fd7f47e513
akupiec <kupiecart@gmail.com>
2024-10-15 21:33:15 +0200

    my config

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ mod error;
 mod models;
 mod services;
 
+#[web::get("/health")]
+async fn health_check() -> web::HttpResponse {
+  web::HttpResponse::Ok().finish()
+}
+
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
+  env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
   web::server(|| {
     web::App::new()
-      // Register swagger endpoints
+      .wrap(web::middleware::Logger::default())
       .configure(services::openapi::ntex_config)
-      // Register todo endpoints
       .configure(services::todo::ntex_config)
-      // Default endpoint for unregisterd endpoints
+      .service(health_check)
       .default_service(web::route().to(services::default))
   })
   .bind(("0.0.0.0", 8080))?
